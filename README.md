aeab5d7dc402d1902cad51fd01e41c3e60a010c7
mollifier <mollifier@gmail.com>
2015-06-12 18:30:02 +0900

    ec2 listコマンドを追加

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A Hubot script to start or stop ec2 virtual machines
 
-See [`src/ec2-vm-scheduler.coffee`](src/ec2-vm-scheduler.coffee) for full documentation.
+See [`src/ec2-vm-scheduler.js`](src/ec2-vm-scheduler.js) for full documentation.
 
 ## Installation
 
@@ -18,9 +18,17 @@ Then add **hubot-ec2-vm-scheduler** to your `external-scripts.json`:
 ]
 ```
 
+## Configuration
+
+```
+export HUBOT_EC2_VM_SCHEDULER_ACCESS_KEY_ID="AXXXX"
+export HUBOT_EC2_VM_SCHEDULER_SECRET_ACCESS_KEY="Yyyyy8"
+```
+
 ## Run in locall shell
 
 ```
+% npm install
 % export PORT=50125
 % PATH=./node_modules/hubot/node_modules/.bin:$PATH $(npm bin)/hubot -a shell -n hubot -r src
 ```
--- src/ec2-vm-scheduler.js
+++ src/ec2-vm-scheduler.js
@@ -3,6 +3,8 @@
 //
 // Configuration:
 //   LIST_OF_ENV_VARS_TO_SET
+//   HUBOT_EC2_VM_SCHEDULER_ACCESS_KEY_ID
+//   HUBOT_EC2_VM_SCHEDULER_SECRET_ACCESS_KEY
 //
 // Commands:
 //   hubot hello - <what the respond trigger does>
@@ -15,10 +17,54 @@
 //   mollifier <mollifier@gmail.com>
 
 module.exports = function(robot) {
-  robot.respond(/ec2\s+start\s+(\S+)/, function(res) {
+  var AWS = require('aws-sdk');
+  AWS.config.region = 'ap-northeast-1';
+  AWS.config.update({
+    accessKeyId: process.env.HUBOT_EC2_VM_SCHEDULER_ACCESS_KEY_ID,
+    secretAccessKey: process.env.HUBOT_EC2_VM_SCHEDULER_SECRET_ACCESS_KEY
+  });
+
+  robot.respond(/ec2\s+list$/, function(res) {
+    var ec2 = new AWS.EC2();
+    ec2.describeInstances({}, function(err, data) {
+      if (err) {
+        res.send("Could not list instances : " + err);
+        return;
+      }
+
+      for (var i = 0; i < data.Reservations.length; i++) {
+        var r = data.Reservations[i];
+        for (var j = 0; j < r.Instances.length; j++) {
+          var instance = r.Instances[j];
+          res.send(instance.InstanceId + " " + instance.KeyName + " " + instance.State.Name);
+        }
+      }
+
+    });
+  });
+
+  robot.respond(/ec2\s+start\s+(\S+)$/, function(res) {
     var name = res.match[1];
     // 仮実装
-    res.send('world ' + name);
+
+    // Class: AWS.EC2 — AWS SDK for JavaScript
+    // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html
+    var ec2 = new AWS.EC2();
+
+    var params = {
+      ImageId: 'xxx'
+    };
+
+    ec2.startInstances(params, function(err, data) {
+      if (err) {
+        res.send("Could not start instance", err);
+        return;
+      }
+      res.send('start ' + name);
+    });
+
+    res.send('starting ' + name + ' ...');
   });
+
 };
 
