dafb14fcae5bb8ffd8ad5a9358ce4f6bc506f3ed
mollifier <mollifier@gmail.com>
2015-06-12 18:39:03 +0900

    設定を追加

--- src/ec2-vm-scheduler.js
+++ src/ec2-vm-scheduler.js
@@ -3,6 +3,8 @@
 //
 // Configuration:
 //   LIST_OF_ENV_VARS_TO_SET
+//   HUBOT_EC2_VM_SCHEDULER_ACCESS_KEY_ID
+//   HUBOT_EC2_VM_SCHEDULER_SECRET_ACCESS_KEY
 //
 // Commands:
 //   hubot hello - <what the respond trigger does>
@@ -15,6 +17,13 @@
 //   mollifier <mollifier@gmail.com>
 
 module.exports = function(robot) {
+  var AWS = require('aws-sdk');
+  AWS.config.region = 'ap-northeast-1';
+  AWS.config.update({
+    accessKeyId: process.env.HUBOT_EC2_VM_SCHEDULER_ACCESS_KEY_ID,
+    secretAccessKey: process.env.HUBOT_EC2_VM_SCHEDULER_SECRET_ACCESS_KEY
+  });
+
   robot.respond(/ec2\s+start\s+(\S+)/, function(res) {
     var name = res.match[1];
     // 仮実装
