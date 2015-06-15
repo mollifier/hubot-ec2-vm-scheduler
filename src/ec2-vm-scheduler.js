f01072d3c2437b2712af84be399b463cf571d2a7
mollifier <mollifier@gmail.com>
2015-06-15 16:26:38 +0900

    EC2仮想マシンリスト インスタンスIDを表示する

--- src/ec2-vm-scheduler.js
+++ src/ec2-vm-scheduler.js
@@ -36,7 +36,7 @@ module.exports = function(robot) {
         var r = data.Reservations[i];
         for (var j = 0; j < r.Instances.length; j++) {
           var instance = r.Instances[j];
-          res.send(instance.Name + " " + instance.State);
+          res.send(instance.InstanceId + " " + instance.State.Name);
         }
       }
 
